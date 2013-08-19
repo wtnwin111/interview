4457ba07d494d4266dc3315cbac9566dd179de62
Zhile Zou <zouzhile@gmail.com>
2013-08-19 21:40:23 +0800

    console writer to print 2-d array

--- src/com/interview/utils/ConsoleWriter.java
+++ src/com/interview/utils/ConsoleWriter.java
@@ -7,4 +7,12 @@ public class ConsoleWriter {
 		}
 		System.out.println();
 	}
+
+    public static void printIntArray(int[][] array) {
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j< array.length; j ++)
+                System.out.print("\t" + array[i][j]);
+            System.out.println();
+        }
+    }
 }
