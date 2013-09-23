b2362e055b0fdb5f8d91cdcf26e057963f8d735d
StefanieZhao <zhaochenting@gmail.com>
2013-09-24 07:32:54 +0800

    sample graph data

--- src/com/interview/algorithms/graph/GraphSearcher.java
+++ src/com/interview/algorithms/graph/GraphSearcher.java
@@ -11,8 +11,27 @@ import com.interview.datastructures.graph.Graph;
 
 public class GraphSearcher {
 
+    /*
+        1 ----> 2                 7 -> 9
+        |       |                 |    |
+         -> 3    -> 4 <- 5 <- 6 <-     |
+                              |        |
+                               --> 8 <--
+     */
     public Graph generateSampleGraph(){
         Graph graph = new AdjListGraph();
+        graph.type = Graph.DIRECTED;
+
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(5, 4);
+        graph.addEdge(6, 5);
+        graph.addEdge(6, 8);
+        graph.addEdge(7, 6);
+        graph.addEdge(7, 9);
+        graph.addEdge(9, 8);
+
         return graph;
     }
 
