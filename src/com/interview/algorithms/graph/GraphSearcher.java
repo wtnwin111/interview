7647265eebdf58e6aff64a812e5ba18da25530a9
Zhile Zou <zouzhile@gmail.com>
2013-09-23 17:12:17 +0800

    graph searcher

---
+++ src/com/interview/algorithms/graph/GraphSearcher.java
@@ -0,0 +1,28 @@
+package com.interview.algorithms.graph;
+
+import com.interview.datastructures.graph.AdjListGraph;
+import com.interview.datastructures.graph.Graph;
+
+/**
+ * Created_By: zouzhile
+ * Date: 9/23/13
+ * Time: 5:05 PM
+ */
+
+public class GraphSearcher {
+
+    public Graph generateSampleGraph(){
+        Graph graph = new AdjListGraph();
+        return graph;
+    }
+
+    public void doDepthFirstSearch(Graph graph) {
+
+    }
+
+    public void doBreadthFirstSearch(Graph graph) {
+
+    }
+
+
+}
