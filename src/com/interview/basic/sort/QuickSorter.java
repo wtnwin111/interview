1ff635293bbd3f36e1cfe24567c33be9996ff643
StefanieZhao <zhaochenting@gmail.com>
2013-12-21 18:18:33 +0800

    complete sort part

---
+++ src/com/interview/basic/sort/QuickSorter.java
@@ -0,0 +1,53 @@
+package com.interview.basic.sort;
+
+import java.util.Random;
+
+public class QuickSorter extends Sorter{
+
+	@Override
+	public int[] sort(int[] input) {
+		input = shuffle(input);
+		sort(input, 0, input.length - 1);
+		return input;
+	}
+	
+	public void sort(int[] input, int lo, int hi){
+		if(hi <= lo) return;
+		//partition array and return the key index
+		int key = partition(input, lo, hi);
+		//sort the sub array
+		sort(input, lo, key - 1);
+		sort(input, key + 1, hi);
+		
+	}
+	
+	public int partition(int[] input, int lo, int hi){
+		int key = input[lo];
+		int i = lo, j = hi + 1;
+		while(true){
+			// find item on lo to swap
+			while(input[++i] < key)
+				if(i == hi) break;
+			// find item on hi to swap
+			while(input[--j] > key)
+				if(j == lo) break;
+			// check if pointers cross
+			if (i >= j) break;
+			swap(input, i, j);
+		}
+		//with a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+		swap(input, lo, j);
+		return j;
+	}
+	
+	public int[] shuffle(int[] input){
+		int N = input.length;
+		Random random = new Random();
+        for (int i = 0; i < N; i++) {
+            int r = i + random.nextInt(N-i);     // between i and N-1
+            swap(input, i, r);
+        }
+        return input;
+	}
+
+}
--- test/com/interview/basic/sort/TestSorter.java
+++ test/com/interview/basic/sort/TestSorter.java
@@ -40,5 +40,39 @@ public class TestSorter {
 		System.out.print("Shell Sort Result:\t");
 		ConsoleWriter.printIntArray(sortedArray);
 	}
-
+	
+	@Test
+	public void testMergeSort(){
+		Sorter sorter = new MergeSorter();
+		int[] sortedArray = sorter.sort(TestUtil.copyArray(testArray));
+		System.out.print("Merge Sort Result:\t");
+		ConsoleWriter.printIntArray(sortedArray);
+	}
+	
+	@Test
+	public void testQuickSort(){
+		Sorter sorter = new QuickSorter();
+		int[] sortedArray = sorter.sort(TestUtil.copyArray(testArray));
+		System.out.print("Quick Sort Result:\t");
+		ConsoleWriter.printIntArray(sortedArray);
+	}
+	
+	@Test
+	public void testHeapUpSort(){
+		Sorter sorter = new HeapSorter();
+		int[] array = new int[] {66, 32, 73, 32, 50, 33, 30, 16, 40, 21};
+		int[] sortedArray = sorter.sort(array);
+		System.out.print("Heap Up Sort Result:\t");
+		ConsoleWriter.printIntArray(sortedArray);
+	}
+	
+	@Test
+	public void testHeapDownSort(){
+		HeapSorter sorter = new HeapSorter();
+		sorter.up = false;
+		int[] array = new int[] {66, 32, 73, 32, 50, 33, 30, 16, 40, 21};
+		int[] sortedArray = sorter.sort(array);
+		System.out.print("Heap Down Sort Result:\t");
+		ConsoleWriter.printIntArray(sortedArray);
+	}
 }
