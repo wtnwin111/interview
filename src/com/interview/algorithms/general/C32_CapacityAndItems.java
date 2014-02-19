214cd32c5b2db9bc14bd2c13326b388d744defe1
Zhile Zou <zouzhile@yahoo-inc.com>
2014-02-19 17:01:33 +0800

    calculate combinations of the items for the given capacity

--- src/com/interview/algorithms/general/C32_CapacityAndItems.java
+++ src/com/interview/algorithms/general/C32_CapacityAndItems.java
@@ -1,5 +1,7 @@
 package com.interview.algorithms.general;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  * User: stefanie
@@ -14,9 +16,7 @@ package com.interview.algorithms.general;
 public class C32_CapacityAndItems {
 
     public boolean check(int N, int[] packs) {
-
         return check(N, 0, packs);
-
     }
 
     public boolean check(int N, int currentSum, int[] packs) {
@@ -37,18 +37,60 @@ public class C32_CapacityAndItems {
      * @param packs
      * @return
      */
-    public int[] getPacksAmount(int N, int[] packs) {
-
-        return null;
+    public void getPacksAmount(int N, int[] packs) {
+        int[] packsCount = new int[packs.length];
+        getPacksCount(N, packs, 0, packsCount);
     }
 
-    public int[] getPacksCount(int N, int[] packs, int currentSum, int[] packsCount) {
-        return null;
+    public int getPacksCount(int N, int[] packs, int currentSum, int[] packsCount) {
+
+        if(currentSum > N)
+            return 1;
+
+        if(currentSum == N) {
+            printCombination(packs, packsCount, currentSum);
+            return 0;
+        }
+
+
+        // current sum < N, try to put in more packs
+        // the input packsCount shouldn't be changed on return
+        int[] packsCountCopy = Arrays.copyOf(packsCount, packsCount.length);
+        for(int i = 0; i < packs.length; i ++) {
+            // put in pack[i]
+            packsCountCopy[i] += 1;
+            currentSum += packs[i];
+
+            int status = this.getPacksCount(N, packs, currentSum, packsCountCopy);
+            if(status >= 0) {
+                // when the last pack make the sum equal or bigger than N,
+                // move out the last pack to try other packs
+                currentSum -= packs[i];
+                packsCountCopy[i] -= 1; // take out pack[i]
+            }
+        }
+        return -1;
+    }
+
+    private void printCombination(int[] packs, int[] packsCount, int sum) {
+        for(int i = 0; i < packs.length; i ++)
+            System.out.print("\t" + packs[i] + ":" + packsCount[i]);
+        System.out.print("\tsum=" + sum);
+        System.out.println();
     }
 
 
     public static void main(String[] args) {
-
+        C32_CapacityAndItems checker = new C32_CapacityAndItems();
+        int N = 42;
+        int[] packs = new int[]{6, 9, 20};
+        String packString = "";
+        for(int pack : packs)
+            packString += pack + " ";
+        System.out.println("N = " + N + ", packs = " + packString);
+        System.out.println("Can buy: " + checker.check(N, packs));
+        System.out.println("Combinations:");
+        checker.getPacksAmount(N, packs);
     }
 
 }
