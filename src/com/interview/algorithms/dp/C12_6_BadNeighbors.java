fdede262d90c662f762ad123aa1e8268e936be52
Summer Zhao <zhaochenting@gmail.com>
2014-06-25 16:53:02 +0800

    Refine the code of Dynamic Programing C12_5_ZigZag and C12_6_BadNeighbors 1. refine the code style for ZigZag 2. add another DP implementation for C12_6_BadNeighbors

--- src/com/interview/algorithms/dp/C12_6_BadNeighbors.java
+++ src/com/interview/algorithms/dp/C12_6_BadNeighbors.java
@@ -21,7 +21,7 @@ package com.interview.algorithms.dp;
  */
 public class C12_6_BadNeighbors {
 
-    public int maxDonations(int[] donations, int from, int to) {
+    public int maxDonationsByRecursion(int[] donations, int from, int to) {
         int max = Integer.MIN_VALUE;
 
         if(to - from < 3) {
@@ -43,12 +43,49 @@ public class C12_6_BadNeighbors {
         }
         // select the "from" donation
         if(from == 0 && to == donations.length - 1)
-            max = donations[from] + maxDonations(donations, from + 2, to - 1);
+            max = donations[from] + maxDonationsByRecursion(donations, from + 2, to - 1);
         else
-            max = donations[from] + maxDonations(donations, from + 2, to);
+            max = donations[from] + maxDonationsByRecursion(donations, from + 2, to);
 
         // don't select "from" donation
-        int max1 = maxDonations(donations, from + 1, to);
+        int max1 = maxDonationsByRecursion(donations, from + 1, to);
         return (max > max1 ? max : max1);
+    }
+
+    /**
+     * Besides the last one:
+     *  opt[S] = max( opt[S-1], opt[S-2] + donations[S])
+     * For the last one and if 1st one also selected
+     *  opt[S] = max( opt[S-1], opt[S-2] - donations[0] + max( donations[S], donations[0])
+     *
+     */
+    public int maxDonationsDP(int[] donations){
+        int N = donations.length;
+        int optimal[] = new int[N];
+        //isFirstIn is used to determine when visit the last one, since the town is a circle.
+        boolean isFirstIn[] = new boolean[N];
+
+        //init the state
+        optimal[0] = donations[0];
+        isFirstIn[0] = true;
+        optimal[1] = donations[1];
+        isFirstIn[1] = false;
+
+        for(int i = 2; i < N; i++){
+            int addOpt;
+            if(i == N - 1 && isFirstIn[i-2]){  //when visit the last one, need select 1st or last in.
+                addOpt = optimal[i-2] - donations[0] + Math.max(donations[0], donations[i]);
+            } else {
+                addOpt = optimal[i-2] + donations[i];
+            }
+            if(addOpt > optimal[i-1]){
+                optimal[i] = addOpt;
+                isFirstIn[i] = isFirstIn[i-2];
+            } else {
+                optimal[i] = optimal[i-1];
+                isFirstIn[i] = isFirstIn[i-1];
+            }
+        }
+        return optimal[N-1];
     }
 }
--- test/com/interview/algorithms/dp/C12_6_BadNeighborsTest.java
+++ test/com/interview/algorithms/dp/C12_6_BadNeighborsTest.java
@@ -15,20 +15,25 @@ public class C12_6_BadNeighborsTest {
         C12_6_BadNeighbors test = new C12_6_BadNeighbors();
 
         int[] donations = new int[] { 10, 3, 2, 5, 7, 8 };
-        Assert.assertEquals(test.maxDonations(donations, 0, donations.length - 1), 19);
+        Assert.assertEquals(test.maxDonationsByRecursion(donations, 0, donations.length - 1), 19);
+        Assert.assertEquals(test.maxDonationsDP(donations), 19);
 
         donations = new int[] { 11, 15};
-        Assert.assertEquals(test.maxDonations(donations, 0, donations.length - 1), 15);
+        Assert.assertEquals(test.maxDonationsByRecursion(donations, 0, donations.length - 1), 15);
+        Assert.assertEquals(test.maxDonationsDP(donations), 15);
 
         donations = new int[] { 7, 7, 7, 7, 7, 7, 7 };
-        Assert.assertEquals(test.maxDonations(donations, 0, donations.length - 1), 21);
+        Assert.assertEquals(test.maxDonationsByRecursion(donations, 0, donations.length - 1), 21);
+        Assert.assertEquals(test.maxDonationsDP(donations), 21);
 
         donations = new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
-        Assert.assertEquals(test.maxDonations(donations, 0, donations.length - 1), 16);
+        Assert.assertEquals(test.maxDonationsByRecursion(donations, 0, donations.length - 1), 16);
+        Assert.assertEquals(test.maxDonationsDP(donations), 16);
 
         donations = new int[] { 94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61,
                 6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
                 52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72 };
-        Assert.assertEquals(test.maxDonations(donations, 0, donations.length - 1), 2926);
+        Assert.assertEquals(test.maxDonationsByRecursion(donations, 0, donations.length - 1), 2926);
+        Assert.assertEquals(test.maxDonationsDP(donations), 2926);
     }
 }
