302134ee88702655daf61f61d7c6c3812e446f67
StefanieZhao <zhaochenting@gmail.com>
2014-06-26 17:31:17 +0800

    Add 3 DP problem

---
+++ src/com/interview/algorithms/dp/C12_12_JewelrySplit.java
@@ -0,0 +1,34 @@
+package com.interview.algorithms.dp;
+
+/**
+ * Created by chenting on 2014/6/26.
+ * You have been given a list of jewelry items that must be split amongst two people: Frank and Bob. Frank likes very expensive jewelry.
+ * Bob doesn't care how expensive the jewelry is, as long as he gets a lot of jewelry.
+ * Based on these criteria you have devised the following policy:
+ *
+ * 1) Each piece of jewelry given to Frank must be valued greater than or equal to each piece of jewelry given to Bob.
+ *    In other words, Frank's least expensive piece of jewelry must be valued greater than or equal to Bob's most expensive piece of jewelry.
+ * 2) The total value of the jewelry given to Frank must exactly equal the total value of the jewelry given to Bob.
+ * 3) There can be pieces of jewelry given to neither Bob nor Frank.
+ * 4) Frank and Bob must each get at least 1 piece of jewelry.
+ * Given the value of each piece, you will determine the number of different ways you can allocate the jewelry to
+ * Bob and Frank following the above policy.
+ *
+ * For example:
+ * values = {1,2,5,3,4,5}
+ * Valid allocations are:
+ * Bob       		Frank
+ * 1,2		         3
+ * 1,3        		 4
+ * 1,4		         5  (first 5)
+ * 1,4         		 5  (second 5)
+ * 2,3 		         5  (first 5)
+ * 2,3         		 5  (second 5)
+ * 5  (first 5)		 5  (second 5)
+ * 5  (second 5)	 5  (first 5)
+ * 1,2,3,4       		5,5
+ * Note that each '5' is a different piece of jewelry and needs to be accounted for separately.
+ * There are 9 legal ways of allocating the jewelry to Bob and Frank given the policy, so your method would return 9.
+ */
+public class C12_12_JewelrySplit {
+}
---
+++ src/com/interview/algorithms/dp/C12_14_QuickSums.java
@@ -0,0 +1,16 @@
+package com.interview.algorithms.dp;
+
+/**
+ * Created by chenting on 2014/6/26.
+ *
+ * Given a string of digits, find the minimum number of additions required for the string to equal some target number.
+ * Each addition is the equivalent of inserting a plus sign somewhere into the string of digits. After all plus signs are inserted,
+ * evaluate the sum as usual. For example, consider the string "12" (quotes for clarity). With zero additions, we can achieve the number 12.
+ * If we insert one plus sign into the string, we get "1+2", which evaluates to 3. So, in that case, given "12", a minimum of 1 addition is required to
+ * get the number 3. As another example, consider "303" and a target sum of 6. The best strategy is not "3+0+3", but "3+03".
+ * You can do this because leading zeros do not change the result.
+ * Write a class QuickSums that contains the method minSums, which takes a String numbers and an int sum. The method should calculate and return the
+ * minimum number of additions required to create an expression from numbers that evaluates to sum. If this is impossible, return -1.
+ */
+public class C12_14_QuickSums {
+}
