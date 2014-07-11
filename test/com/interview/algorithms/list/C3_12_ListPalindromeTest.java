42847becaededef7ffccdc299458204654efa6c3
Summer Zhao <zhaochenting@gmail.com>
2014-07-11 14:02:00 +0800

    Add implementation of C3_12_ListPalindrome using Stack and Recursive

---
+++ test/com/interview/algorithms/list/C3_12_ListPalindromeTest.java
@@ -0,0 +1,44 @@
+package com.interview.algorithms.list;
+
+import com.interview.basics.model.collection.LinkedList;
+import junit.framework.TestCase;
+
+public class C3_12_ListPalindromeTest extends TestCase {
+
+    public void testcase1(){
+        LinkedList<String> list = new LinkedList<>();
+        String[] nodes = new String[] {"a","b", "c", "b", "a"};
+        for(String str : nodes) list.add(str);
+
+        assertEquals(true, C3_12_ListPalindrome.isParlindromeByStack(list));
+        assertEquals(true, C3_12_ListPalindrome.isParlindromeByRecursive(list));
+    }
+
+    public void testcase2(){
+        LinkedList<String> list = new LinkedList<>();
+        String[] nodes = new String[] {"a","b", "c", "c", "b", "a"};
+        for(String str : nodes) list.add(str);
+
+        assertEquals(true, C3_12_ListPalindrome.isParlindromeByStack(list));
+        assertEquals(true, C3_12_ListPalindrome.isParlindromeByRecursive(list));
+    }
+
+    public void testcase3(){
+        LinkedList<String> list = new LinkedList<>();
+        String[] nodes = new String[] {"a","b", "c", "b", "a", "e"};
+        for(String str : nodes) list.add(str);
+
+        assertEquals(false, C3_12_ListPalindrome.isParlindromeByStack(list));
+        assertEquals(false, C3_12_ListPalindrome.isParlindromeByRecursive(list));
+    }
+
+    public void testcase4(){
+        LinkedList<String> list = new LinkedList<>();
+        String[] nodes = new String[] {"a"};
+        for(String str : nodes) list.add(str);
+
+        assertEquals(true, C3_12_ListPalindrome.isParlindromeByStack(list));
+        assertEquals(true, C3_12_ListPalindrome.isParlindromeByRecursive(list));
+    }
+
+}
