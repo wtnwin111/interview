035e05404fc0ec46efb4f2f89e5339ba14a602a5
Summer Zhao <zhaochenting@gmail.com>
2014-07-25 13:05:30 +0800

    Add implementation of C1_37_NContainsOneNumber

--- src/com/interview/algorithms/general/C1_37_NContainsOneNumber.java
+++ src/com/interview/algorithms/general/C1_37_NContainsOneNumber.java
@@ -6,46 +6,94 @@ package com.interview.algorithms.general;
  * Time: 下午10:08
  */
 public class C1_37_NContainsOneNumber {
-    public static int number(int N){
-        return number(N, 1, 0, 1, 0, 0);
-    }
+//    public static int number(int N){
+//        return number(N, 1, 0, 1, 0, 0);
+//    }
 
-    private static int number(int N, int exp, int lower, int times, int low_count, int full_count){
-        int mod = N % 10;
-        int count = 0;
-        if(mod > 1) {
-            count+= exp;
-            full_count = count;
+    public static int number(int N) {
+        int SIZE = 10;
+        int[] digits = new int[SIZE];
+        int[] full_count = new int[SIZE];
+        int[] low_number = new int[SIZE];
+
+        int times = 1;
+        int base = N;
+        int i = 1;
+        full_count[0] = 0;
+        full_count[1] = 1;
+        low_number[0] = 0;
+        while (base > 0) {
+            digits[i] = base % 10;
+            low_number[i] = digits[i] * times;
+            if (i > 0) {
+                low_number[i] += low_number[i - 1];
+                full_count[i] = 10 * full_count[i - 1] + times;
+            }
+            base = base / 10;
+            times = times * 10;
+            i++;
         }
-        else if(mod == 1) count+= lower+1+low_count+full_count;
-        else if(mod < 1) count+= low_count;
-        if(N/10 > 0){
-            lower = mod * times + lower;
-            count+= number(N/10, exp*10, lower, times+1, count, full_count) * exp;
+
+        int count = 0;
+        int j = 1;
+        times = 1;
+        while (j < i) {
+            if (digits[j] > 1) {
+                count += times + digits[j] * full_count[j - 1];
+            } else if (digits[j] == 1) {
+                count += low_number[j - 1] + 1 + full_count[j - 1];
+            }
+            times = times * 10;
+            j++;
         }
         return count;
     }
 
-    public static int countOf1s(int n) {
-        int[] prefix = new int[10];
-        int[] suffix = new int[10];
-        int[] digits = new int[10]; //10 is enough for 32bit integers int i=0;
-        int base = 1;
-        int i = 0;
-        while (base < n) {
-            suffix[i] = n % base;
-            digits[i] = (n % (base * 10)) - suffix[i];
-            prefix[i] = (n - suffix[i] - digits[i]*base)/10;
-            i++;
-            base*=10;
-        }
+    public static int numberRecursive(int n){
+        return number(n, 1, 0, 0);
+    }
+
+    private static int number(int n, int times, int low_number, int full_count) {
+        int mod = n % 10;
         int count = 0;
-        base = 1;
-        for (int j=0; j<i; j++) {
-            if (digits[j] < 1) count += prefix[j];
-            else if (digits[j]==1) count += prefix[j] + suffix[j] + 1;
-            else count += prefix[j] + base;
-            base *= 10;
+        if (n / 10 > 0) {
+            count += number(n / 10, times * 10, mod * times + low_number, 10 * full_count + times);
+        }
+        if (mod > 1) {
+            count += times + mod * full_count;
+        } else if (mod == 1) {
+            count += low_number + 1 + full_count;
+        }
+        return count;
+    }
+
+    public static int numberLoop(int n){
+        int count = 0;
+        int times = 1;
+        int low_number = 0;
+        int full_count = 0;
+        while(n > 0){
+            int mod = n % 10;
+            if (mod > 1) {
+                count += times + mod * full_count;
+            } else if (mod == 1) {
+                count += low_number + 1 + full_count;
+            }
+            n = n /10;
+            low_number = mod * times + low_number;
+            full_count = 10 * full_count + times;
+            times = times * 10;
+        }
+        return count;
+    }
+
+    public static int correctAnswer(int N) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            String st = String.valueOf(i);
+            for (int j = 0; j < st.length(); j++) {
+                if (st.charAt(j) == '1') count++;
+            }
         }
         return count;
     }
--- test/com/interview/algorithms/general/C1_37_NContainsOneNumberTest.java
+++ test/com/interview/algorithms/general/C1_37_NContainsOneNumberTest.java
@@ -1,5 +1,6 @@
 package com.interview.algorithms.general;
 
+import com.interview.util.TestUtil;
 import junit.framework.TestCase;
 
 /**
@@ -9,18 +10,52 @@ import junit.framework.TestCase;
  */
 public class C1_37_NContainsOneNumberTest extends TestCase {
     public void testcase1(){
+//        System.out.println(C1_37_NContainsOneNumber.correctAnswer(9));
+//        System.out.println(C1_37_NContainsOneNumber.correctAnswer(99));
+//        System.out.println(C1_37_NContainsOneNumber.correctAnswer(999));
+//        System.out.println(C1_37_NContainsOneNumber.correctAnswer(12));
+
         assertEquals(5, C1_37_NContainsOneNumber.number(12));
+        assertEquals(5, C1_37_NContainsOneNumber.numberRecursive(12));
+        assertEquals(5, C1_37_NContainsOneNumber.numberLoop(12));
     }
 
     public void testcase2(){
+        //System.out.println(C1_37_NContainsOneNumber.correctAnswer(10));
         assertEquals(2, C1_37_NContainsOneNumber.number(10));
+        assertEquals(2, C1_37_NContainsOneNumber.numberRecursive(10));
+        assertEquals(2, C1_37_NContainsOneNumber.numberLoop(10));
     }
 
     public void testcase3(){
+        //System.out.println(C1_37_NContainsOneNumber.correctAnswer(11));
         assertEquals(4, C1_37_NContainsOneNumber.number(11));
+        assertEquals(4, C1_37_NContainsOneNumber.numberRecursive(11));
+        assertEquals(4, C1_37_NContainsOneNumber.numberLoop(11));
     }
 
     public void testcase4(){
-        assertEquals(29, C1_37_NContainsOneNumber.number(111));
+        //System.out.println(C1_37_NContainsOneNumber.correctAnswer(111));
+        assertEquals(36, C1_37_NContainsOneNumber.number(111));
+        assertEquals(36, C1_37_NContainsOneNumber.numberRecursive(111));
+        assertEquals(36, C1_37_NContainsOneNumber.numberLoop(111));
+    }
+
+    public void testcase5(){
+        System.out.println(C1_37_NContainsOneNumber.correctAnswer(101));
+        assertEquals(23, C1_37_NContainsOneNumber.number(101));
+        assertEquals(23, C1_37_NContainsOneNumber.numberRecursive(101));
+        assertEquals(23, C1_37_NContainsOneNumber.numberLoop(101));
+    }
+
+    public void testRandom(){
+        for(int i = 0; i < 10; i++){
+            int number = TestUtil.generateInt(10000);
+            int ca = C1_37_NContainsOneNumber.correctAnswer(number);
+
+            assertEquals(ca, C1_37_NContainsOneNumber.number(number));
+            assertEquals(ca, C1_37_NContainsOneNumber.numberRecursive(number));
+            assertEquals(ca, C1_37_NContainsOneNumber.numberLoop(number));
+        }
     }
 }
