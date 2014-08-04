ac06b9f45b6f7ddce4a43d1275393435f91eb8b6
Summer Zhao <zhaochenting@gmail.com>
2014-08-04 13:28:39 +0800

    Add problem for N Sieves: C1_44_NSieves

---
+++ test/com/interview/algorithms/general/C1_44_NSievesTest.java
@@ -0,0 +1,14 @@
+package com.interview.algorithms.general;
+
+import junit.framework.TestCase;
+
+public class C1_44_NSievesTest extends TestCase {
+
+    public void testSumPossibility() throws Exception {
+        C1_44_NSieves.sumPossibility(1);
+    }
+
+    public void testSumPossibility2() throws Exception {
+        C1_44_NSieves.sumPossibility(2);
+    }
+}
