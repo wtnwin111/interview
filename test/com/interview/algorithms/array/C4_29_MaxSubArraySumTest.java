d1535e20f20a3ff4923540edabda22b14a0e2e9a
Summer Zhao <zhaochenting@gmail.com>
2014-09-23 23:33:09 +0800

    Add problems: 1. A solution forward scan for C4_29_MaxSubArraySum 2. return range in C4_29_MaxSubArraySum

--- test/com/interview/algorithms/array/C4_29_MaxSubArraySumTest.java
+++ test/com/interview/algorithms/array/C4_29_MaxSubArraySumTest.java
@@ -13,6 +13,13 @@ public class C4_29_MaxSubArraySumTest extends TestCase {
         int[] array = new int[] {1, -2, 3, 10, -4, 7, 2, -5};
         int sum = C4_29_MaxSubArraySum.max(array);
         assertEquals(18, sum);
+        sum = C4_29_MaxSubArraySum.maxForword(array);
+        assertEquals(18, sum);
+        C4_29_MaxSubArraySum.Range range = C4_29_MaxSubArraySum.maxRange(array);
+        assertEquals(18, range.sum);
+        assertEquals(2, range.begin);
+        assertEquals(6, range.end);
+
     }
 
     public void testcase2() throws Exception {
@@ -20,5 +27,37 @@ public class C4_29_MaxSubArraySumTest extends TestCase {
         int sum = C4_29_MaxSubArraySum.max(array);
         int max = 10 + 33 - 23 + 32 - 12 + 41;
         assertEquals(max, sum);
+        sum = C4_29_MaxSubArraySum.maxForword(array);
+        assertEquals(max, sum);
+        C4_29_MaxSubArraySum.Range range = C4_29_MaxSubArraySum.maxRange(array);
+        assertEquals(max, range.sum);
+        assertEquals(1, range.begin);
+        assertEquals(6, range.end);
+    }
+
+    public void testcase3() throws Exception {
+        int[] array = new int[] {14, 10, 33, 23, 32, 12, 41, 12, 12};
+        int sum = C4_29_MaxSubArraySum.max(array);
+        int max = 0;
+        for(int i = 0; i < array.length; i++) max += array[i];
+        assertEquals(max, sum);
+        sum = C4_29_MaxSubArraySum.maxForword(array);
+        assertEquals(max, sum);
+        C4_29_MaxSubArraySum.Range range = C4_29_MaxSubArraySum.maxRange(array);
+        assertEquals(max, range.sum);
+        assertEquals(0, range.begin);
+        assertEquals(8, range.end);
+    }
+
+    public void testcase4() throws Exception {
+        int[] array = new int[] {-14, -10, -33, -23, -2, -12, -41, -12, -12};
+        int sum = C4_29_MaxSubArraySum.max(array);
+        assertEquals(-2, sum);
+        sum = C4_29_MaxSubArraySum.maxForword(array);
+        assertEquals(-2, sum);
+        C4_29_MaxSubArraySum.Range range = C4_29_MaxSubArraySum.maxRange(array);
+        assertEquals(-2, range.sum);
+        assertEquals(4, range.begin);
+        assertEquals(4, range.end);
     }
 }
