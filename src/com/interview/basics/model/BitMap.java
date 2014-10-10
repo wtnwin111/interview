1405cb0433f66fa1cbd53499a882818a8d4e5ac4
Summer Zhao <zhaochenting@gmail.com>
2014-10-10 17:59:58 +0800

    An enhanced implementation of BitMap

---
+++ src/com/interview/basics/model/BitMap.java
@@ -0,0 +1,90 @@
+package com.interview.basics.model;
+
+/**
+ * Created_By: stefanie
+ * Date: 14-10-10
+ * Time: 下午5:10
+ */
+public class BitMap {
+    public static int DEFAULT_SIZE = 1024;
+    public static int UNIT_SIZE = 32;
+
+    private int size = DEFAULT_SIZE;
+    private int[] map;
+
+    public BitMap(int size) {
+        this.size = size;
+        int count = (int) Math.ceil(1.0 * size / UNIT_SIZE);
+        map = new int[count];
+    }
+
+    private void checkIndex(int i) throws Exception{
+        if(i >= size) throw new Exception("BitMap Index Overflow");
+    }
+
+    public boolean getBit(int i) throws Exception{
+        checkIndex(i);
+        int offset = i / UNIT_SIZE;
+        int index = i % UNIT_SIZE;
+        return ((map[offset] & ( 1 << index)) != 0);
+    }
+
+    public void setBit(int i) throws Exception{
+        checkIndex(i);
+        int offset = i / UNIT_SIZE;
+        int index = i % UNIT_SIZE;
+        map[offset] |= (1 << index);
+    }
+
+    public void clearBit(int i) throws Exception{
+        checkIndex(i);
+        int offset = i / UNIT_SIZE;
+        int index = i % UNIT_SIZE;
+        int mask = ~(1 << index);
+        map[offset] &= mask;
+    }
+
+    public void clearRightBit(int i) throws Exception{
+        checkIndex(i);
+        int offset = i / UNIT_SIZE;
+        int index = i % UNIT_SIZE;
+        int mask = ~((1 << (index + 1)) - 1);
+        map[offset] &= mask;
+        for(int j = 0; j < offset; j++) map[j] &= 0;
+    }
+
+    public void clearLeftBit(int i) throws Exception{
+        checkIndex(i);
+        int offset = i / UNIT_SIZE;
+        int index = i % UNIT_SIZE;
+        int mask = (1 << (index + 1)) - 1;
+        map[offset] &= mask;
+        for(int j = offset + 1; j < map.length; j++) map[j] &= 0;
+    }
+
+    public void updateBit(int i, boolean flag) throws Exception{
+        checkIndex(i);
+        int offset = i / UNIT_SIZE;
+        int index = i % UNIT_SIZE;
+        int mask = ~(1 << index);
+        map[offset] &= mask;
+        if(flag) map[offset] |= (1 << index);
+    }
+
+    public void print(){
+        for(int i = 0; i < map.length; i++){
+            System.out.print(toBinary(map[i]));
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    private String toBinary(int value) {
+        StringBuilder binary = new StringBuilder();
+        for(int j = UNIT_SIZE - 1; j >= 0; j--) {
+            binary.append(value & 1);
+            value = value >>> 1;
+        }
+        return binary.toString();
+    }
+}
---
+++ test/com/interview/basics/model/BitMapTest.java
@@ -0,0 +1,44 @@
+package com.interview.basics.model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created_By: stefanie
+ * Date: 14-10-10
+ * Time: 下午5:43
+ */
+public class BitMapTest extends TestCase {
+
+    public void testcase() throws Exception {
+        BitMap map = new BitMap(64);
+
+        map.setBit(10);
+        map.print();
+
+        map.setBit(36);
+        map.print();
+
+        map.setBit(32);
+        map.print();
+
+        map.clearBit(32);
+        map.print();
+
+        map.clearLeftBit(20);
+        map.print();
+
+        map.clearRightBit(20);
+        map.print();
+
+        map.setBit(47);
+        map.print();
+
+        assertTrue(map.getBit(47));
+
+        map.updateBit(38, true);
+        map.updateBit(47, false);
+        map.print();
+
+        assertFalse(map.getBit(47));
+    }
+}
