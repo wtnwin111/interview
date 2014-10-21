6b453cb61f6e85aa1ace08ea6873c37614806ee0
Summer Zhao <zhaochenting@gmail.com>
2014-10-21 15:09:16 +0800

    Add the deletion in RedBlackTree (un-finished)

--- src/com/interview/basics/model/tree/RedBlackTree.java
+++ src/com/interview/basics/model/tree/RedBlackTree.java
@@ -7,20 +7,21 @@ package com.interview.basics.model.tree;
  * Time: 4:19 PM
  */
 public class RedBlackTree<T extends Comparable<T>> extends BinarySearchTree<T> {
-    static class RedBlackTreeNode<T extends Comparable<T>> extends BinaryTreeNode<T>{
+    static class RedBlackTreeNode<T extends Comparable<T>> extends BinaryTreeNode<T> {
         public static final boolean BLACK = false;
         public static final boolean RED = true;
         public boolean color = RED;
+
         public RedBlackTreeNode(T value) {
             super(value);
         }
 
-        public boolean isRed(BinaryTreeNode node){
+        public boolean isRed(BinaryTreeNode node) {
             return ((node != null) && node instanceof RedBlackTreeNode
-                    && ((RedBlackTreeNode)node).color == RED);
+                    && ((RedBlackTreeNode) node).color == RED);
         }
 
-        public RedBlackTreeNode rotateRight(){
+        public RedBlackTreeNode rotateRight() {
             RedBlackTreeNode tmp = (RedBlackTreeNode) this.left;
             this.setLeft(tmp.right);
             tmp.setRight(this);
@@ -29,7 +30,7 @@ public class RedBlackTree<T extends Comparable<T>> extends BinarySearchTree<T> {
             return tmp;
         }
 
-        public RedBlackTreeNode rotateLeft(){
+        public RedBlackTreeNode rotateLeft() {
             RedBlackTreeNode tmp = (RedBlackTreeNode) this.right;
             this.setRight(tmp.left);
             tmp.setLeft(this);
@@ -38,10 +39,10 @@ public class RedBlackTree<T extends Comparable<T>> extends BinarySearchTree<T> {
             return tmp;
         }
 
-        public void flipColors(){
+        public void flipColors() {
             this.color = RED;
-            ((RedBlackTreeNode)this.left).color = BLACK;
-            ((RedBlackTreeNode)this.right).color = BLACK;
+            ((RedBlackTreeNode) this.left).color = BLACK;
+            ((RedBlackTreeNode) this.right).color = BLACK;
         }
     }
 
@@ -53,22 +54,91 @@ public class RedBlackTree<T extends Comparable<T>> extends BinarySearchTree<T> {
         super(root);
     }
 
+    private RedBlackTreeNode fixUp(RedBlackTreeNode node) {
+        if (node.isRed(node.right) && !node.isRed(node.left)) node = node.rotateLeft();
+        if (node.isRed(node.left) && node.isRed(node.left.left)) node = node.rotateRight();
+        if (node.isRed(node.left) && node.isRed(node.right)) node.flipColors();
+        return node;
+    }
+
     @Override
     protected BinaryTreeNode<T> insert(BinaryTreeNode<T> node, T element) {
-        if(node == null)    return new RedBlackTreeNode<>(element);
+        if (node == null) return new RedBlackTreeNode<>(element);
         int cmp = node.value.compareTo(element);
         if (cmp == 0) node.count++;
         else if (cmp > 0) node.setLeft(insert(node.left, element));
         else node.setRight(insert(node.right, element));
-        RedBlackTreeNode bnode = (RedBlackTreeNode) node;
-        if (bnode.isRed(bnode.right) && !bnode.isRed(bnode.left)) node = bnode.rotateLeft();
-        if (bnode.isRed(bnode.left) && bnode.isRed(bnode.left.left)) node = bnode.rotateRight();
-        if (bnode.isRed(bnode.left) && bnode.isRed(bnode.right)) bnode.flipColors();
-        return node;
+        return fixUp((RedBlackTreeNode) node);
     }
 
-//    @Override
-//    protected BinaryTreeNode<T> delete(BinaryTreeNode<T> node, T element){
+//    private RedBlackTreeNode<T> moveRedRight(RedBlackTreeNode<T> node) {
+//        if (node.isRed(node.left) && node.isRed(node.right)) node.flipColors();
+//        if (node.left != null && node.isRed(node.left.left)) {
+//            node = node.rotateRight();
+//            if (node.isRed(node.left) && node.isRed(node.right)) node.flipColors();
+//        }
+//        return node;
+//    }
 //
+//    @Override
+//    public void deleteMax() {
+//        root = deleteMax((RedBlackTreeNode) root);
+//        ((RedBlackTreeNode) root).color = RedBlackTreeNode.BLACK;
+//    }
+//
+//    protected RedBlackTreeNode deleteMax(RedBlackTreeNode<T> node) {
+//        if(node == null) return null;
+//        if (node.isRed(node.left))  node = node.rotateRight();
+//        if (node.right == null)     return (RedBlackTreeNode)node.left;
+//        if (!node.isRed(node.right) && !node.isRed(node.right.left))
+//            node = moveRedRight(node);
+//        node.left = deleteMax((RedBlackTreeNode)node.left);
+//        return fixUp(node);
+//    }
+//
+//    private RedBlackTreeNode moveRedLeft(RedBlackTreeNode node){
+//        if (node.isRed(node.left) && node.isRed(node.right)) node.flipColors();
+//        if (node.right != null && node.isRed(node.right.left)){
+//            node.right = ((RedBlackTreeNode)node.right).rotateRight();
+//            node = node.rotateLeft();
+//            if (node.isRed(node.left) && node.isRed(node.right)) node.flipColors();
+//        }
+//        return node;
+//    }
+//
+//    @Override
+//    public void deleteMin() {
+//        root = deleteMin((RedBlackTreeNode) root);
+//        ((RedBlackTreeNode) root).color = RedBlackTreeNode.BLACK;
+//    }
+//
+//    protected RedBlackTreeNode<T> deleteMin(RedBlackTreeNode<T> node) {
+//        if (node.left == null)  return (RedBlackTreeNode)node.right;
+//        if (!node.isRed(node.left) && node.left != null && !node.isRed(node.left.left))
+//            node = moveRedLeft(node);
+//        node.left = deleteMin((RedBlackTreeNode)node.left);
+//        return fixUp(node);
+//    }
+//
+//    @Override
+//    protected BinaryTreeNode<T> delete(BinaryTreeNode<T> node, T element) {
+//        if(node == null) return null;
+//        int cmp = node.value.compareTo(element);
+//        RedBlackTreeNode h = (RedBlackTreeNode)node;
+//        if (cmp > 0){
+//            if (!h.isRed(h.left) && !h.isRed(h.left.left))
+//                h = moveRedLeft(h);
+//            h.left = delete(h.left, element);
+//        } else {
+//            if (h.isRed(h.left)) h = moveRedRight(h);
+//            if (cmp == 0 && (h.right == null))   return null;
+//            if (!h.isRed(h.right) && !h.isRed(h.right.left))
+//                h = moveRedRight(h);
+//            if (cmp == 0){
+//                h.value = min(h.right);
+//                h.right = deleteMin((RedBlackTreeNode)h.right);
+//            }   else h.right = delete(h.right, element);
+//        }
+//        return fixUp(h);
 //    }
 }
--- test/com/interview/basics/model/tree/RedBlackTreeTest.java
+++ test/com/interview/basics/model/tree/RedBlackTreeTest.java
@@ -1,5 +1,6 @@
 package com.interview.basics.model.tree;
 
+import com.interview.utils.BinaryTreePrinter;
 import junit.framework.TestCase;
 
 public class RedBlackTreeTest extends TestCase {
@@ -60,16 +61,25 @@ public class RedBlackTreeTest extends TestCase {
     public void testDelete(){
         Integer[] data = new Integer[]{15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9};
         RedBlackTree<Integer> tree = new RedBlackTree<Integer>(data);
+        BinaryTreePrinter.print(tree.root);
         tree.resize();
         assertEquals(2, tree.min().value.intValue());
         tree.deleteMin();
+        BinaryTreePrinter.print(tree.root);
         assertEquals(3, tree.min().value.intValue());
+        tree.deleteMin();
+        BinaryTreePrinter.print(tree.root);
+        assertEquals(4, tree.min().value.intValue());
         assertEquals(20, tree.max().value.intValue());
         tree.deleteMax();
+        BinaryTreePrinter.print(tree.root);
         assertEquals(18, tree.max().value.intValue());
 
         assertEquals(13, tree.ceil(12).value.intValue());
         tree.delete(13);
+        BinaryTreePrinter.print(tree.root);
         assertEquals(15, tree.ceil(12).value.intValue());
+
+        BinaryTreePrinter.print(tree.root);
     }
 }
