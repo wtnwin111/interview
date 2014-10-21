789dbad58443f401a1345949390bd86276a3f837
Summer Zhao <zhaochenting@gmail.com>
2014-10-21 13:35:03 +0800

    Another solution for C4_70_PartialSortedArrayOffsetFinder

--- test/com/interview/algorithms/array/C4_70_PartialSortedArrayOffsetFinderTest.java
+++ test/com/interview/algorithms/array/C4_70_PartialSortedArrayOffsetFinderTest.java
@@ -23,4 +23,19 @@ public class C4_70_PartialSortedArrayOffsetFinderTest extends TestCase {
         assertEquals(3, indices.begin);
         assertEquals(9, indices.end);
     }
+    public void testFindAsc2() throws Exception {
+        int[] array = new int[]{1,2,4,7,10,11,7,12,6,7,16,18,19};
+        C4_70_PartialSortedArrayOffsetFinder2.Indices indices = C4_70_PartialSortedArrayOffsetFinder2.find(array);
+
+        assertEquals(3, indices.begin);
+        assertEquals(9, indices.end);
+    }
+
+    public void testFindDec2() throws Exception {
+        int[] array = new int[]{19,18,16,7,6,12,7,11,10,7,4,2,1};
+        C4_70_PartialSortedArrayOffsetFinder2.Indices indices = C4_70_PartialSortedArrayOffsetFinder2.find(array);
+
+        assertEquals(3, indices.begin);
+        assertEquals(9, indices.end);
+    }
 }
