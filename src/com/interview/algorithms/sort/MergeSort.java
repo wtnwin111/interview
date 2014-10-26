b3e19b83cd230617c88904bb637f914b7cccd113
Zhile Zou <zouzhile@yahoo-inc.com>
2014-10-26 17:49:16 +0800

    median tracker with heap (priority queue) implementation

--- src/com/interview/algorithms/sort/MergeSort.java
+++ src/com/interview/algorithms/sort/MergeSort.java
@@ -1,80 +1,46 @@
 package com.interview.algorithms.sort;
 
-import java.util.Arrays;
-
-import com.interview.utils.ConsoleReader;
-
-
+/**
+ * Created_By: zouzhile
+ * Date: 10/19/14
+ * Time: 2:41 PM
+ */
 public class MergeSort {
 
-	public int[] sort(int[] array, int begin, int end){
-		
-		if (begin < end){ // there are at least 2 elements from begin to end
-			int index = (begin + end)/2;
-			array = this.sort(array, begin, index);
-			array = this.sort(array, index + 1, end);
-			array = this.merge(array, begin, index, end);
-		} 
-				
-		return array;
-	}
-	
-	/**
-	 * Apply merge for two sub arrays bounded by begin, middle and end
-	 * @param array
-	 * @param begin
-	 * @param middle
-	 * @param end
-	 * @return the array after this merge
-	 */
-	public int[] merge(int[] array, int begin, int middle, int end) {
-		int[] result = Arrays.copyOf(array, array.length);
-		// The initial value of i and j must make sure the max differences of 
-		// the 2 sub arrays' sizes must be at most 1
-		int i = begin, j = middle + 1;  
-		int offset = 0;
-		while(i <= middle && j <= end) {
-			if(array[i] <= array[j]) {
-				result[begin + offset] = array[i];
-				i++; 
-			} else {
-				result[begin + offset] = array[j];
-				j++;
-			}
-			offset ++;
-		} 
-		while(i <= middle) {
-			result[begin + offset] = array[i];
-			i ++;
-			offset ++;
-		}
-		
-		while(j <= end) {
-			result[begin + offset] = array[j];
-			j ++;
-			offset ++;
-		}
-		
-		return result;
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("The Merge Sort Algorithm Implementation");
-		System.out.println("===========================================================");
-		
-		ConsoleReader reader = new ConsoleReader();
-		System.out.print("Please input the int array elements: ");
-		int[] array = reader.readIntItems();
+    private int[] aux;
 
-		MergeSort sorter = new MergeSort();
-		array = sorter.sort(array, 0, array.length - 1);
-		//array = sorter.merge(array, 0, 2, 5);
-		System.out.print("The merge sort result is: ");
-		for(int value : array)
-			System.out.print(value + " ");
-	}
+    private void sort(int[] array) {
+        aux = new int[array.length];
+        this.sort(array, 0, array.length - 1);
+    }
 
+    public void sort(int[] array, int lo, int hi) {
+        if(lo >= hi)
+            return;
+        int mid = (lo + hi) / 2;
+        sort(array, lo, mid);
+        sort(array, mid+1, hi);
+        merge(array, lo, mid, hi);
+    }
+
+    public void merge(int[] array, int lo, int mid, int hi) {
+        for(int i = lo; i <= hi; i ++) {
+            aux[i] = array[i];
+        }
+        int i = lo, j = mid + 1;
+        for(int k = lo; k <= hi; k ++) {
+            if(i > mid) array[k] = aux[j++];
+            else if(j > hi) array[k] = aux[i++];
+            else if(aux[i] < aux[j]) array[k] = aux[i++];
+            else array[k] = aux[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {4, 5, 1, 2 ,3};
+        MergeSort sorter = new MergeSort();
+        sorter.sort(array);
+        for(int a : array)
+            System.out.print(a + " ");
+    }
 }
