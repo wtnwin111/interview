925f66c1937d098ba67ee4b69e82455c1755ef47
Summer Zhao <zhaochenting@gmail.com>
2014-10-30 10:34:32 +0800

    Refine the solution of C5_14_CommonAncestor and C5_16_TreeSumPath

--- src/com/interview/algorithms/tree/C5_14_CommonAncestor.java
+++ src/com/interview/algorithms/tree/C5_14_CommonAncestor.java
@@ -11,18 +11,6 @@ import com.interview.basics.model.tree.BinaryTreeNode;
  */
 public class C5_14_CommonAncestor {
 
-    public static BinaryTreeNode find(BinaryTree tree, BinaryTreeNode n1, BinaryTreeNode n2){
-        if(n1 == n2) return n1;
-        else {
-            if(n1.height < n2.height)
-                return find(tree, n1.parent, n2);
-            else if(n1.height > n2.height)
-                return find(tree, n1, n2.parent);
-            else
-                return find(tree, n1.parent, n2.parent);
-        }
-    }
-
     public static BinaryTreeNode getLCA(BinaryTree tree, BinaryTreeNode n1, BinaryTreeNode n2){
         return getLCA(tree.getRoot(), n1, n2);
     }
@@ -33,12 +21,23 @@ public class C5_14_CommonAncestor {
         BinaryTreeNode left = getLCA(root.left, n1, n2);
         BinaryTreeNode right = getLCA(root.right, n1, n2);
         /*
-            if left == null, return right as root
-            if left != null and right == null, return left as root
-            if left != null and right != null, return parent as root
+            if left and right contains both the two nodes, return the root
+            if left or right contains one of the nodes, return it.
          */
         if (left == null) return right;
         else if (right == null) return left;
         else return root;
+    }
+
+    public static BinaryTreeNode find(BinaryTree tree, BinaryTreeNode n1, BinaryTreeNode n2){
+        if(n1 == n2) return n1;
+        else {
+            if(n1.height < n2.height)
+                return find(tree, n1.parent, n2);
+            else if(n1.height > n2.height)
+                return find(tree, n1, n2.parent);
+            else
+                return find(tree, n1.parent, n2.parent);
+        }
     }
 }
