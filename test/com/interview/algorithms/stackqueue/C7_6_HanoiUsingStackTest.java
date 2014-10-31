105a65d8cc642459379738633886d2fe347bbe4e
Summer Zhao <zhaochenting@gmail.com>
2014-10-31 15:42:45 +0800

    Refine the code of HanoiTower: C7_6_HanoiUsingStack

--- test/com/interview/algorithms/stackqueue/C7_6_HanoiUsingStackTest.java
+++ test/com/interview/algorithms/stackqueue/C7_6_HanoiUsingStackTest.java
@@ -1,18 +1,23 @@
 package com.interview.algorithms.stackqueue;
 
-import com.interview.basics.model.collection.list.List;
 import junit.framework.TestCase;
+
+import java.util.List;
 
 public class C7_6_HanoiUsingStackTest extends TestCase {
 
     public void test3Disk() throws Exception {
-        List<Step> steps = C7_6_HanoiUsingStack.solve(3);
+        C7_6_HanoiUsingStack game = new C7_6_HanoiUsingStack();
+        List<C7_6_HanoiUsingStack.Step> steps = game.solve(3);
         assertEquals(7, steps.size());
+        for(C7_6_HanoiUsingStack.Step step : steps) System.out.println(step.toString());
     }
 
     public void test5Disk(){
-        List<Step> steps = C7_6_HanoiUsingStack.solve(5);
+        C7_6_HanoiUsingStack game = new C7_6_HanoiUsingStack();
+        List<C7_6_HanoiUsingStack.Step> steps = game.solve(5);
         assertEquals(31, steps.size());
+        for(C7_6_HanoiUsingStack.Step step : steps) System.out.println(step.toString());
     }
 
 }
