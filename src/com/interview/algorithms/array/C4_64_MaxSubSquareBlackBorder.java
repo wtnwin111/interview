c70d88b8a7b63b90c0d2238aefa628820c043135
Zhile Zou <zouzhile@yahoo-inc.com>
2014-11-01 22:05:21 +0800

    black border

--- src/com/interview/algorithms/array/C4_64_MaxSubSquareBlackBorder.java
+++ src/com/interview/algorithms/array/C4_64_MaxSubSquareBlackBorder.java
@@ -7,63 +7,61 @@ package com.interview.algorithms.array;
  */
 public class C4_64_MaxSubSquareBlackBorder {
 
-    static class Result {
-        int x = 0;
-        int y = 0;
-        int columns = 0;
-        int rows = 0;
+    class Result {
+        int x1, y1; // left up corner point
+        int x2, y2; // bottom right corner point
+        int size() { return (x2 - x1) * (y2 - y1); }
     }
 
-    public static Result find(int[][] array) {
+    public Result find(boolean[][] array) {
         Result result = new Result();
-        for (int i = 0; i < array.length - 1; i++) // rows
-            for (int j = 0; j < array[0].length - 1; j++) // columns
-                find(array, i, j, result);
+        for(int i = 0; i < array.length; i ++)
+            for(int j = 0; j < array[0].length; j ++)
+                if(array[i][j])
+                    this.find(array, i, j, result);
         return result;
     }
 
-    private static void find(int[][] array, int x, int y, Result result) {
-        if (array[x][y] == 0 || array[x + 1][y] == 0 || array[x][y + 1] == 0)
-            return;
-        int rowSpan = 0;
-        int columnSpan = 0;
+    public void find(boolean[][] array, int x1, int y1, Result result) {
+        int y2 = y1;
+        while(y2 < array[0].length && array[x1][y2]) y2++;
+        if(y2 == array[0].length || !array[x1][y2]) y2 --;
 
-        // going top to down
-        while (rowSpan < array.length - 1 && array[x + rowSpan][y] == 1)
-            rowSpan += 1;
+        int x2 = x1;
+        while(x2 < array.length && array[x2][y1]) x2++;
+        if(x2 == array.length || !array[x2][y1]) x2 --;
 
-        // going left to right
-        while (columnSpan < array[0].length - 1 && array[x][y + columnSpan] == 1)
-            columnSpan += 1;
-
-        for (int i = 1; i <= rowSpan; i++) {
-            for (int j = 1; j <= columnSpan; j++)
-                find(array, x, y, x + i, y + j, result);
+        for(int i = x1; i <= x2; i ++) {
+            if(! array[i][y2]) {
+                y2 -= 1;
+                i = x1;
+            }
         }
 
-    }
+        for(int j = y1; j <= y2; j ++) {
+            if(! array[x2][j]) {
+                x2 -= 1;
+                j = y1;
+            }
+        }
 
-
-    private static void find(int[][] array, int x, int y, int x1, int y1, Result result) {
-        for (int i = x1; i > x; i--)
-            if (array[i][y1] == 0)
-                return;
-        for (int j = y1; j > y; j--)
-            if (array[x1][j] == 0)
-                return;
-
-        if ((x1 - x + 1) * (y1 - y + 1) > result.columns * result.rows) {
-            result.x = x;
-            result.y = y;
-            result.columns = y1 - y + 1;
-            result.rows = x1 - x + 1;
+        if((y2 - y1) * (x2 - x1) > result.size()) {
+            result.x1 = x1;
+            result.y1 = y1;
+            result.x2 = x2;
+            result.y2 = y2;
         }
     }
 
     public static void main(String[] args) {
-        int[][] array = new int[][]{{0, 1, 1, 1, 0}, {0, 1, 0, 1, 0}, {0, 1, 1, 1, 0}};
-        Result result = find(array);
-        System.out.println(String.format("x=%s, y=%s, rows=%s, columns=%s", result.x, result.y, result.rows, result.columns));
+        // int[][] array = new int[][] {{0, 1, 1, 1, 0}, {0, 1, 0, 1, 0}, {0, 1, 1, 1, 0}};
+        boolean[][] array = new boolean[][] {
+                {false, true, true,  true, true},
+                {false, true, false, true, true},
+                {false, true, true,  true, true}};
+        C4_64_MaxSubSquareBlackBorder finder = new C4_64_MaxSubSquareBlackBorder();
+        Result border = finder.find(array);
+        System.out.println(String.format("x1=%s, y1=%s, x2=%s,y2=%s", border.x1, border.y1, border.x2, border.y2));
     }
 
 }
