67aaa5cadd71ef965580df02495c2cb3ba017da9
Summer Zhao <zhaochenting@gmail.com>
2014-11-09 21:11:30 +0800

    Refine the code to be more readiable

--- test/com/interview/algorithms/string/C11_36_DecodeWaysTest.java
+++ test/com/interview/algorithms/string/C11_36_DecodeWaysTest.java
@@ -9,13 +9,13 @@ import junit.framework.TestCase;
  */
 public class C11_36_DecodeWaysTest extends TestCase {
     public void testNumDecodings() throws Exception {
-        assertEquals(0, C11_36_DecodeWays.numDecoding("00"));
-        assertEquals(0, C11_36_DecodeWays.numDecoding("01"));
-        assertEquals(0, C11_36_DecodeWays.numDecoding("40"));
-        assertEquals(0, C11_36_DecodeWays.numDecoding(""));
-        assertEquals(1, C11_36_DecodeWays.numDecoding("10"));
-        assertEquals(1, C11_36_DecodeWays.numDecoding("110"));
-        assertEquals(3, C11_36_DecodeWays.numDecoding("124"));
+        assertEquals(0, C11_36_DecodeWays.numDecodings("00"));
+        assertEquals(0, C11_36_DecodeWays.numDecodings("01"));
+        assertEquals(0, C11_36_DecodeWays.numDecodings("40"));
+        assertEquals(0, C11_36_DecodeWays.numDecodings(""));
+        assertEquals(1, C11_36_DecodeWays.numDecodings("10"));
+        assertEquals(1, C11_36_DecodeWays.numDecodings("110"));
+        assertEquals(3, C11_36_DecodeWays.numDecodings("124"));
 
     }
 }
