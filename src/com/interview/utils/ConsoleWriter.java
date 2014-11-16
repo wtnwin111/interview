09381e11dd6d6fb39dc1552ce8f4fdb7bb13fa50
Summer Zhao <zhaochenting@gmail.com>
2014-11-16 21:32:17 +0800

    code for SudokuGame

--- src/com/interview/utils/ConsoleWriter.java
+++ src/com/interview/utils/ConsoleWriter.java
@@ -30,6 +30,14 @@ public class ConsoleWriter {
         }
     }
 
+    public static void printIntArray(char[][] array) {
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j< array[0].length; j ++)
+                System.out.print("\t" + array[i][j]);
+            System.out.println();
+        }
+    }
+
     public static void print(List<List<Integer>> cols){
         for(List<Integer> item : cols){
             for(Integer number : item){
