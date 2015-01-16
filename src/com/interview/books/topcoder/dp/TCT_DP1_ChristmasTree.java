5b7766b3a8081956c0ddc21665993a332c7ab64a
Summer Zhao <zhaochenting@gmail.com>
2015-01-16 23:17:59 +0800

    Add leetcodeoj problems

---
+++ src/com/interview/books/topcoder/dp/TCT_DP1_ChristmasTree.java
@@ -0,0 +1,12 @@
+package com.interview.books.topcoder.dp;
+
+/**
+ * Created_By: stefanie
+ * Date: 15-1-16
+ * Time: 下午9:26
+ */
+public class TCT_DP1_ChristmasTree {
+    public long decorationWays(int level, int red, int blue, int green){
+        return 0;
+    }
+}
